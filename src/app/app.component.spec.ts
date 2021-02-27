8da519a6d0c2982396ba9597f4d9dfcc1cf353ba
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-02-27 01:34:52 -0400

    Refatora testes iniciais

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-contacts app is running!');
+    expect(compiled.querySelector('.toolbar span').textContent).toContain('CONTATOS');
   });
 });
